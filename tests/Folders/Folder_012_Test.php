b6d7e283d16a1e8b2f7a05ebfa6f6eed945686ff
Richardas Kuchinskas <36065987+Hidanio@users.noreply.github.com>
2024-02-01 16:42:17 +0300

    added new test cases from kPHP (#46)

---
+++ tests/Folders/Folder_012_Test.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ModuliteTests\Folders;
+
+use ModuliteTests\RunAllRulesForFolderTestCase;
+
+
+class Folder_012_Test extends RunAllRulesForFolderTestCase {
+  static function getProjectFolderName(): string {
+    return '012_package_not_required_in_derived_class';
+  }
+}
---
+++ tests/Folders/Folder_013_Test.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ModuliteTests\Folders;
+
+use ModuliteTests\RunAllRulesForFolderTestCase;
+
+
+class Folder_013_Test extends RunAllRulesForFolderTestCase {
+  static function getProjectFolderName(): string {
+    return '013_inheritance_calling_class_inside_parent';
+  }
+}
